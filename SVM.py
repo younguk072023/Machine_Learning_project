ec9a1fb25a3fa508ba403a88339443ea27323167
park_younguk <young072023@naver.com>
2025-10-23 10:51:58 +0900

    change dataset

--- SVM.py
+++ SVM.py
@@ -1,34 +1,24 @@
-import pandas as pd
-
+from datasets import load_dataset
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text  import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from sklearn.metrics import  accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report
 
-
-# Tf는 하나의 문서에서 특정 단어가 등장하는 횟수
-# Idf 역수를 취해서 적은 문장에 등장할수록 큰 숫자가 되게하고 반대로 많은 문서에 등장할수록 숫자를 작아지게 함으로써 문장에서 의미없이 사용되는 단어의 가중치를 줄임.
-
-df = pd.read_csv("data/mtsamples.csv")
-
-
-df = df.dropna(subset=["transcription", "medical_specialty"])   #결측값 제거
+dataset = load_dataset("ag_news")
+texts = dataset["train"]["text"]
+labels = dataset["train"]["label"]
 
 x_train, x_test, y_train, y_test = train_test_split(
-    df["transcription"], df["medical_specialty"],test_size=0.2,random_state=42
+    texts, labels, test_size=0.2, random_state=42
 )
-
 
 vectorizer = TfidfVectorizer(max_features=5000, stop_words="english")
 Xtr = vectorizer.fit_transform(x_train)
 Xte = vectorizer.transform(x_test)
 
-svm_model = SVC(kernel = "linear")
+svm_model = SVC(kernel="linear")
 svm_model.fit(Xtr, y_train)
 pred = svm_model.predict(Xte)
 
 print("TF-IDF + SVM 정확도:", round(accuracy_score(y_test, pred), 3))
 print(classification_report(y_test, pred))
-                                                  
-    
-
